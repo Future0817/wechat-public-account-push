36c9929f3656c1220b216b879be71c1b0ac01124
Future0817 <118792409+Future0817@users.noreply.github.com>
2022-11-22 14:31:03 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,10 +8,10 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID:wxc249a4de8ecb56f9'',
+  APP_ID: 'wxc249a4de8ecb56f9',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET:20d901fa91d5aa584536663125930a96 '',
+  APP_SECRET: '20d901fa91d5aa584536663125930a96',
 
   PROVINCE: '河南',
   CITY: '周口',
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id:oTAMz57MeZ1Ugio1VwNFh6Dp5sRM'',
+      id: 'oTAMz57MeZ1Ugio1VwNFh6Dp5sRM',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId:_-1AtKOmkWYLQX2CGOLrKJsa9s6-9bGk1i3eZV3lZ0I '',
+      useTemplateId: '_-1AtKOmkWYLQX2CGOLrKJsa9s6-9bGk1i3eZV3lZ0I',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
@@ -49,13 +49,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'wxc249a4de8ecb56f9',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oTAMz53SU40Z3OVDU5eMJDwebGgA',
     }
   ],
 
